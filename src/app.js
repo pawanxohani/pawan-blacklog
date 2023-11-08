c8ba6f8ddae62583e852fa3ece1a1c792d97525c
pawanxohani <pawan.d@xohani.com>
2023-11-08 16:55:41 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("../src/db/connection");
 const router = require("./router/router");
+var cors = require('cors')
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.use(cors({origin : '*'}))
 app.use(express.json());
 app.use("/api/v1/", router);
 
